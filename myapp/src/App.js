beaa4d277d1f5f0dbc03c6f8a3c65a5b1bf722cd
manory zimik <95528408+asmonorizimik@users.noreply.github.com>
2022-12-16 12:26:07 +0530

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import React, {useEffect,useState} from 'react'
+// import { add } from 'lodash';
 const _=require('lodash')
 function App() {
   const [data,setData]  = useState([])
   const [search, setSearch] = useState();
-  const [searchData,setSearchData]=useState([])
+  const [addCart,setAddCart]=useState(0)
   const [myCart,setMyCart]=useState([])
   
   const getData=async()=>{
@@ -17,62 +18,78 @@ function App() {
 
 
 
-  // const searchData = !search ? data:data.filter((item) =>
-  //      item.strCategory.toLowerCase().includes(search.toLowerCase())||item.strMeal.toLowerCase().includes(search.toLowerCase())
-  //   );
+  const searchData = !search ? data:data.filter((item) =>
+       item.strCategory.toLowerCase().includes(search.toLowerCase())||item.strMeal.toLowerCase().includes(search.toLowerCase())
+    );
 
 
   useEffect(()=>{
     getData()
   },[])
-  console.log(data)
   
-  const searchFunction=()=>{
-    if (search.length>3){
-      let searchItem =_.filter(data,{"strCategory":search})
-      setSearchData(searchItem)
-    }
-  }
+  // const searchFunction=()=>{
+  //   if (search.length>3){
+  //     let searchItem =_.filter(data,{"strCategory":search})
+  //     setSearchData(searchItem)
+  //   }
+  // }
 
 
   return (
 
     <div className='app'>
       <h1>Welcome to TheMealDB</h1>
-    <input type='text' placeholder='type here...' onChange={(event)=>{
+     Search : <input type='text' placeholder='type here...' onChange={(event)=>{
         setSearch(event.target.value)
-        searchFunction()
+        // searchFunction()
       }}></input>
+      <button onClick={()=>{
+        alert("cart")
+      }}>My-Cart</button><button>{addCart}</button>
      
 
     <div className="main">
       {
-        search ? searchData.map((item,index)=>{
-          return (
-            <div className='item-display'>
-              <img src={item.strMealThumb} alt=''></img>
-              <p>Category : {item.strCategory}</p>
-              <p>{item.strMeal}   <button>10$</button>   <button>add to cart</button></p>
+        // search ? searchData.map((item,index)=>{
+        //   return (
+        //     <div className='item-display'>
+        //       <img src={item.strMealThumb} alt=''></img>
+        //       {/* <p>Tag : {item.strTags}</p> */}
+        //       <p>Category : {item.strCategory}</p>
+        //       <p><u>{item.strMeal}</u>      <button>10$</button>   <button>add to cart</button></p>
               
-            </div>
-          )
-        })
-      :data.map((item,index)=>{
+        //     </div>
+        //   )
+        // })
+      searchData.map((item,index)=>{
         return (
           <div className='item-display'>
             <img src={item.strMealThumb} alt=''></img>
+            {/* <p>Tag : {item.strTags}</p> */}
             <p>Category : {item.strCategory}</p>
-            <p>{item.strMeal}   <button>10$</button>   <button onclick = {()=>{
+            <p><u>{item.strMeal}</u>       <button>10$</button>   <button onClick = {()=>{
+              setAddCart(addCart+1)
               let newData=[...myCart]
               newData.push(item)
               setMyCart(newData)
               console.log(myCart)
-            }}>add to cart</button></p>
+            }}>add to cart</button> <button onClick={()=>{
+              if (addCart===0){
+                setAddCart(0)
+              }
+              else{
+                setAddCart(addCart-1)
+              }
+            }}>remove</button></p>
+
+
             
           </div> 
         )
       })
       }
+
+      
     </div>
     </div>
   );
